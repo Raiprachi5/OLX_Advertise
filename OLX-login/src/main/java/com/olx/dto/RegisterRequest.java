e31ea19279aa8d01820366b9c67aa88aaa057118
Raiprachi5 <90824629+Raiprachi5@users.noreply.github.com>
2021-09-16 13:20:48 +0530

    Add files via upload

---
+++ OLX-login/src/main/java/com/olx/dto/RegisterRequest.java
@@ -0,0 +1,48 @@
+package com.olx.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class RegisterRequest {
+
+	@Override
+	public String toString() {
+		return "RegisterRequest [username=" + username + ", password=" + password + ", firstname=" + firstname
+				+ ", lastname=" + lastname + "]";
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastname() {
+		return lastname;
+	}
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+	private String username;
+	private String password;
+	private String firstname;
+	private String lastname;
+	
+}
---
+++ OLX-login/src/main/java/com/olx/dto/UserDTO.java
@@ -0,0 +1,47 @@
+package com.olx.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class UserDTO {
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getJwtToken() {
+		return jwtToken;
+	}
+	public void setJwtToken(String jwtToken) {
+		this.jwtToken = jwtToken;
+	}
+	@Override
+	public String toString() {
+		return "UserDTO [username=" + username + ", firstName=" + firstName + ", lastName=" + lastName + ", jwtToken="
+				+ jwtToken + "]";
+	}
+	private String username;
+	private String firstName;
+	private String lastName;
+	private String jwtToken;
+	
+}
