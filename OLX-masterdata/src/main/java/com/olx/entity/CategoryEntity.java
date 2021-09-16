97cb73f79243477f6920b8dc6f4b25e69dc0b6fe
Raiprachi5 <90824629+Raiprachi5@users.noreply.github.com>
2021-09-16 13:19:45 +0530

    Add files via upload

---
+++ OLX-masterdata/src/main/java/com/olx/entity/CategoryEntity.java
@@ -0,0 +1,48 @@
+package com.olx.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@Table(name="categories")
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class CategoryEntity {
+	@Override
+	public String toString() {
+		return "CategoryEntity [id=" + id + ", name=" + name + ", description=" + description + "]";
+	}
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	@Id
+	@GeneratedValue(strategy=GenerationType.SEQUENCE)
+	private long id;
+	private String name;
+	private String description;
+}
